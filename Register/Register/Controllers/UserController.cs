d67f62ce80b49b6ed759575501800ac3a8eabd4a
unknown <c1julymind57@iiht.tech>
2022-11-08 21:13:08 +0530

    first commit

---
+++ Register/Register/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Register.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Register.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public readonly UserContext _context;
+        public UserController(IConfiguration config, UserContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+        [AllowAnonymous]
+        [HttpPost("CreateUser")]
+        public IActionResult Create(User user)
+        {
+            try
+            {
+                if (_context.Users.Where(u => u.Email == user.Email).FirstOrDefault() != null)
+                {
+                    return Ok("Already Exists");
+                }
+                user.MemberSince = DateTime.Now;
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return Ok("Success");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [AllowAnonymous]
+        [HttpPost("LoginUser")]
+        public IActionResult Login(Login user)
+        {
+            var userAvailable = _context.Users.Where(u => u.Email == user.Email && u.Pwd == user.Pwd).FirstOrDefault();
+            if(userAvailable!=null)
+            {
+                return Ok(new JwtService(_config).GenerateToken(
+                    userAvailable.UserID.ToString(),
+                    userAvailable.FirstName,
+                    userAvailable.LastName,
+                    userAvailable.Email,
+                    userAvailable.Mobile,
+                    userAvailable.Gender
+                    )
+                );
+            }
+            return Ok("Failure");
+        }
+    }
+}
---
+++ SFNG/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import{HttpClientModule}from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    AuthService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
